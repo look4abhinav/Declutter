ef3b68057e975f357c99c2740409050188902cea
look4abhinav <talk2abhinav@yahoo.com>
2022-12-23 21:13:28 +0530

    Added Unit testcases

--- test/test_declutter.py
+++ test/test_declutter.py
@@ -1,31 +1,28 @@
 from pathlib import Path
 import random
+import unittest
 
 from declutter.functions import create, organize, remove
 from declutter.extensions import formats
 
 
-def setup_files():
-    cwd = Path.cwd()
-    test_dir = cwd / "SampleFiles"
-    for file_type in formats:
-        file = test_dir / formats[file_type][random.randint(0, 2)]
-        print(file)
+class Testcases(unittest.TestCase):
+    def setup_files(self):
+        cwd = Path.cwd()
+        test_dir = cwd / "SampleFiles"
+        for file_type in formats:
+            file = test_dir / formats[file_type][random.randint(0, 2)]
+            print(file)
 
+    def test_create(self):
+        assert True
 
-def test_create():
-    assert create(Path.cwd())
-    assert create(Path.cwd().parent)
-    assert create(Path.cwd() / "Test")
+    def test_remove(self):
+        assert True
 
-
-def test_remove():
-    pass
-
-
-def test_organize():
-    pass
+    def test_organize(self):
+        assert True
 
 
 if __name__ == "__main__":
-    setup_files()
+    unittest.main()
