7a5e43e551aff1ae947adbdd34a3349f0e80a5bb
look4abhinav <talk2abhinav@yahoo.com>
2023-01-12 23:21:37 +0530

    Updated testcases

--- test/test_declutter.py
+++ test/test_declutter.py
@@ -1,28 +1,43 @@
-from pathlib import Path
+import os
 import random
 import unittest
+from pathlib import Path
 
-from declutter.functions import create, organize, remove
 from declutter.extensions import formats
+from declutter.functions import create, organize, remove
 
 
 class Testcases(unittest.TestCase):
-    def setup_files(self):
-        cwd = Path.cwd()
-        test_dir = cwd / "SampleFiles"
+    def setup_files(self, target):
         for file_type in formats:
-            file = test_dir / formats[file_type][random.randint(0, 2)]
-            print(file)
+            filename = (
+                "file"
+                + str(random.randint(0, 2))
+                + "."
+                + formats[file_type][random.randint(0, 2)]
+            )
+            file = target / filename
+            os.system(f"touch {file}")
 
     def test_create(self):
-        assert True
+        path = Path.cwd() / "Declutter"
+        assert create(path)
 
     def test_remove(self):
-        assert True
+        self.test_organize()
+        assert remove()
 
     def test_organize(self):
-        assert True
+        path = Path.cwd()
+        src = path / "SampleFiles"
+        dest = path / "Declutter"
+        os.mkdir(dest)
+        self.test_create()
+        self.setup_files(src)
+        assert organize(src, dest)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    # unittest.main()
+    t = Testcases()
+    t.test_create()
